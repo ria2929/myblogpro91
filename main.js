2723932e0216252dec581c8fba5978a8209c5473
ria2929 <97019184+ria2929@users.noreply.github.com>
2022-02-08 19:14:13 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function adduser() {
+    player1_name = document.getElementById("player1_name_input").value;
+    player2_name = document.getElementById("player2_name_input").value;
+
+    localStorage.setItem("player1_name"), player1_name;
+    localStorage.setItem("player2_name"), player2_name;
+
+    window.location = "index.html"
+}
+
+function send() {
+    number1 = document.getElementById("number1").value;
+    number2 = document.getElementById("number2").value;
+    actual_answer = parseInt(number1) * parseInt(number2);
+}
+
+question_number = "<h4>" + number1 +" X "+ numeber2 +"</h4>";
+
+input_box = "<br>Answer: <input type='text' id='input_check_box'>";
+
+check_button = "<br><br><button class ='btn btn-info' onclick='check()'>"
+
+document.getElementById("number1").value = "";
+document.getElementById("number2").value = "";
+document.getElementById("output").value = row;
